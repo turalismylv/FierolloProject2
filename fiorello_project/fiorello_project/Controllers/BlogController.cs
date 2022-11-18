8595403ac8931530627e7800a37f454b3eee3715
turalismylv <turalhi@code.edu.az>
2022-11-18 22:02:31 +0400

    FierolloPagination

--- fiorello_project/fiorello_project/Controllers/BlogController.cs
+++ fiorello_project/fiorello_project/Controllers/BlogController.cs
@@ -1,4 +1,5 @@
 ﻿using fiorello_project.DAL;
+using fiorello_project.Models;
 using fiorello_project.ViewModels.Blog;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -16,14 +17,46 @@ namespace fiorello_project.Controllers
             _appDbContext = appDbContext;
         }
 
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(BlogIndexViewModel model)
         {
-            var model = new BlogIndexViewModel
+            var pageCount = await GetPageCountAsync(model.Take);
+
+
+            if (model.Page <= 0 || model.Page>pageCount ) return NotFound();
+          
+            var blogs = await PaginateBlogsAsync(model.Page, model.Take);
+
+             model = new BlogIndexViewModel
             {
-                Blogs = await _appDbContext.Blogs.ToListAsync()
+                Blogs = blogs,
+                Page = model.Page,
+                PageCount= pageCount,
+                Take=model.Take
             };
             return View(model);
         }
+
+        private async Task<List<Blog>> PaginateBlogsAsync(int page,int take)
+        {
+
+            return await _appDbContext.Blogs
+                 .OrderByDescending(b => b.Id)
+                 .Skip((page - 1) * take).Take(take)
+                 .ToListAsync();
+
+        }
+
+
+        private async Task<int> GetPageCountAsync(int take)
+        {
+
+            var blogsCount= await _appDbContext.Blogs.CountAsync();
+
+            return (int)Math.Ceiling((decimal)blogsCount / take);
+
+        }
+
+
 
         public async Task<IActionResult> Details(int id)
         {
--- fiorello_project/fiorello_project/ViewModels/Blog/BlogIndexViewModel.cs
+++ fiorello_project/fiorello_project/ViewModels/Blog/BlogIndexViewModel.cs
@@ -3,5 +3,12 @@
     public class BlogIndexViewModel
     {
         public List<Models.Blog> Blogs { get; set; }
+
+        public int Page { get; set; } = 1;
+
+        public int Take { get; set; } = 9;
+
+        public int PageCount { get; set; }
+
     }
 }
